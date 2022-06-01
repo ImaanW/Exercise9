d9a5793ff70eb3ffa6dc76ad1e129f32171c2267
ImaanW <92485815+ImaanW@users.noreply.github.com>
2022-06-01 10:18:12 +0100

    Initial commit

---
+++ types.py
@@ -0,0 +1,11 @@
+first_name = 'Imaan'
+last_name = ' Williams'
+b = 'car'
+c = 'shoes'
+full_name = first_name + last_name
+print(full_name)
+full_name = ['imaan','williams','car','shoes']
+print(full_name)
+#the bracket from the list are also printed
+mydict = {'first':'imaan','last':'williams','vehicle':'car'}
+print(mydict['vehicle'])
